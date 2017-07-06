6b52dcbf4a1a3c2a2ecca7bcf9c1a539bc45ad52
Misha3739 <Fergana12>
2017-07-06 23:21:33 +0300

    Machine-Inbinded  test added

--- LinkedListSerialization.Tests/DeserializationBehaviour.cs
+++ LinkedListSerialization.Tests/DeserializationBehaviour.cs
@@ -7,7 +7,7 @@ namespace LinkedListSerialization.Tests
     public class DeerializationBehaviour
     {
         static string s_file = @"C:\Serialization\TestFileDeser.txt";
-
+        static string s_file_ser_deser = @"C:\Serialization\TestFileSerDeser.txt";
 
         [Test]
         public void CanDeserialize()
@@ -25,6 +25,7 @@ namespace LinkedListSerialization.Tests
         }
 
         [Test]
+        [Ignore("Developer-Machine-binded test")]
         public void CanDeserialize_CorrectItemsCountAndValues()
         {
             var listRand = new ListRand();
@@ -47,6 +48,45 @@ namespace LinkedListSerialization.Tests
             }
         }
 
+
+        [Test]
+        public void CanSerialzize_Deserialize_CorrectItemsCountAndValues()
+        {
+            var listRand = new ListRand();
+            for (int i = 0; i < 50; i++)
+            {
+                listRand.AddNewToTheEnd(new ListNode($"value{i}"));
+            }
+            listRand.Search("value2").Rand = listRand.Search("value6");
+            listRand.Search("value3").Rand = listRand.Search("value7");
+            listRand.Search("value49").Rand = listRand.Search("value3");
+
+            using (FileStream fs = File.Create(s_file_ser_deser))
+            {
+                listRand.Serialize(fs);
+            }
+
+            listRand = new ListRand();
+
+            using (FileStream fs = File.OpenRead(s_file_ser_deser))
+            {
+                listRand.Deserialize(fs);
+            }
+
+
+            Assert.That(listRand.Count, Is.EqualTo(50));
+
+                for (int i = 0; i < 50; i++)
+                {
+                    Assert.That(listRand.Search("value" + i), Is.Not.Null);
+                }
+
+
+                Assert.That(listRand.Search("value2").Rand.Data, Is.EqualTo("value6"));
+                Assert.That(listRand.Search("value3").Rand.Data, Is.EqualTo("value7"));
+                Assert.That(listRand.Search("value49").Rand.Data, Is.EqualTo("value3"));
+            }
+        
 
     }
 }
